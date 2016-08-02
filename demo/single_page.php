ee5bbcb7baa25539fc66b5a155a83a3381c484cc
hegeng <462748757@qq.com>
2016-08-02 14:31:04 +0800

    1

--- demo/single_page.php
+++ demo/single_page.php
@@ -6,8 +6,18 @@ set_include_path(get_include_path() . PATH_SEPARATOR . $demo_include_path);
 require_once('phpfetcher.php');
 class mycrawler extends Phpfetcher_Crawler_Default {
     public function handlePage($page) {
+        echo "+++ enter page: [" . $page->getUrl() . "] +++\n";
+        $objContent = $page->sel("//div");
+        for ($i = 0; $i < count($objContent); ++$i) {
+            $objSpan = $objContent[$i]->find("span");
+            for ($j = 0; $j < count($objSpan); ++$j) {
+                if ($objSpan[$j]->getAttribute('class') == 'cx_price') {
+                    echo $objSpan[$j]->outertext() . "\n";
+                }
+            }
+        }
         //打印处当前页面的title
-        $res = $page->sel('//title');
+        $res = $page->sel('//h1');
         for ($i = 0; $i < count($res); ++$i) {
             echo $res[$i]->plaintext;
             echo "\n";
@@ -20,7 +30,7 @@ $arrJobs = array(
     //任务的名字随便起，这里把名字叫qqnews
     //the key is the name of a job, here names it qqnews
     'qqnews' => array( 
-        'start_page' => 'http://news.qq.com/a/20140927/026557.htm', //起始网页
+        'start_page' => 'http://www.tuniu.com/tours/210204641', //起始网页
         'link_rules' => array(
             /*
              * 所有在这里列出的正则规则，只要能匹配到超链接，那么那条爬虫就会爬到那条超链接
